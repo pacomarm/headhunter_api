5cd5d9c7bc0bdda5f5b2351c8d88ca76fda87fe4
0xshellcode <david.r0sas@protonmail.com>
2021-11-17 23:56:33 -0600

    Add some adjustments to models

--- models/HeadHunter.js
+++ models/HeadHunter.js
@@ -2,11 +2,9 @@ const { Schema, model } = require('mongoose');
 
 const TalentSchema = new Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true },
   email: { type: String, required: true },
   /*password: { type: String, required: true },
   state: { type: String, required: true },
-  country: { type: String, required: true },
   company: { type: String, required: true },
   projects: { type: String, required: true },
   reputation: { type: Number, default: 0 }, */
--- models/Talent.js
+++ models/Talent.js
@@ -3,14 +3,12 @@ const { Schema, model } = require('mongoose');
 const TalentSchema = new Schema({
   id: { type: Number, required: true },
   name: { type: String, required: true },
-  username: { type: String, required: true },
   /*email: { type: String, required: true },
   password: { type: String, required: true },
   state: { type: String, required: true },
-  country: { type: String, required: true },
   reputation: { type: Number, default: 0 },
   profession: { type: String, required: true },
-  habilidades: { type: String, required: true }, */
+  skills: { type: String, required: true }, */
 });
 
 module.exports = model('Talent', TalentSchema);
