b8c89cddac3ef20903af74275897fccd435714a8
0xshellcode <david.r0sas@protonmail.com>
2021-11-18 00:12:17 -0600

    Add more models

--- models/Talent.js
+++ models/Talent.js
@@ -1,10 +1,9 @@
 const { Schema, model } = require('mongoose');
 
 const TalentSchema = new Schema({
-  id: { type: Number, required: true },
   name: { type: String, required: true },
-  /*email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true },
+  /* password: { type: String, required: true },
   state: { type: String, required: true },
   reputation: { type: Number, default: 0 },
   profession: { type: String, required: true },
